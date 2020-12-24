64961eec215ea95936694aedc96f1b383c919580
koteswararao <koteswararao.pinjala@gmail.com>
2020-12-24 11:32:01 +0530

    Add:checking for 21 wins of Heads or Tails

--- flipCoinSimulation.sh
+++ flipCoinSimulation.sh
@@ -1,13 +1,14 @@
 echo "Welcome to Flip Coin Simulation"
 #!/bin/bash -x
 
-#Variables
+#variables
 heads=0
 tails=0
-Toss-$((RANDOM%2))
-for(( i=1;i<=10;i++ ))                          #for loop for generating 10 random numbers
+Toss=$((RANDOM%2))
+while [[ $heads -lt 21 && $tails -lt 21 ]]       #while for heads less than 21 and tails less than 21
 do
-   if [[ $((RANDOM%2)) -eq 0 ]]                 #if condition for random value
+   Toss=$((RANDOM%2))
+   if [[ $Toss -eq 0 ]]                          #if condition for random value
    then
       ((heads++))
    else
