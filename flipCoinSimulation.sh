99215b92f4a4d7511fdc4a20afde384499a31e53
koteswararao <koteswararao.pinjala@gmail.com>
2020-12-24 11:17:43 +0530

    Add:flipping coin for heads or tails

--- flipCoinSimulation.sh
+++ flipCoinSimulation.sh
@@ -1,1 +1,12 @@
 echo "Welcome to Flip Coin Simulation"
+
+#!/bin/bash -x
+
+Toss=$((RANDOM%2))                              #Random value for heads or tails
+
+if [[ $Toss -eq 1 ]]                            #if condition for random value
+then
+   echo "Heads"
+else
+   echo "Tails"
+fi
