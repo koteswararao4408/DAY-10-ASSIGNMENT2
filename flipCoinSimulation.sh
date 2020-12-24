f9602534aa4852b607bac4a06762973fa37f3b5b
koteswararao <koteswararao.pinjala@gmail.com>
2020-12-24 11:26:03 +0530

    Add:flipping the coin multiple times and checking win

--- flipCoinSimulation.sh
+++ flipCoinSimulation.sh
@@ -1,12 +1,18 @@
 echo "Welcome to Flip Coin Simulation"
-
 #!/bin/bash -x
 
-Toss=$((RANDOM%2))                              #Random value for heads or tails
-
-if [[ $Toss -eq 1 ]]                            #if condition for random value
-then
-   echo "Heads"
-else
-   echo "Tails"
-fi
+#Variables
+heads=0
+tails=0
+Toss-$((RANDOM%2))
+for(( i=1;i<=10;i++ ))                          #for loop for generating 10 random numbers
+do
+   if [[ $((RANDOM%2)) -eq 0 ]]                 #if condition for random value
+   then
+      ((heads++))
+   else
+      ((tails++))
+   fi
+done
+echo "Tosses count :" $heads
+echo "Heads count :" $tails
